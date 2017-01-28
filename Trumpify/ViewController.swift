908c151da594bd3533d98fc9cff79ca7375aedd2
Fabian Vergara <vergarf@gmail.com>
2017-01-28 12:13:09 -0500

    Initial Commit

---
+++ Trumpify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trumpify
+//
+//  Created by Fabian Vergara on 2017-01-28.
+//  Copyright © 2017 fvergara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
